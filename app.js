f15de017d2e00577b41a12aa6c780cab4a3226c7
Dalton Tejada <d.tejada@motorcredito.com.do>
2021-10-19 18:18:08 -0400

    agregando  rutra

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const indexRoutes = require('./routers/index')
 app.use(bodyParser.json())
 app.use('/api', indexRoutes);
 
+app.use('/', (req, res) => {
+    res.json({
+        msg: 'ruta por defecto'
+    })
+})
+
 const cors = require('cors');
 // habilitar cors para permitir consultar desde frondend externo
 app.use(cors());
