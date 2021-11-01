e3ce4fbe4984409648ab0cc119c38dcf1317a5fb
Dalton Tejada <d.tejada@motorcredito.com.do>
2021-11-01 11:38:11 -0400

    agregando comentario

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const server = http.createServer(app);
 
 console.log(process.env.PORT);
 
-
+//ESTE CODIGO VERIFICA SI  EXISTEN MODELOS QUE AUN NO EXISTEN EN LA DB.
+// SI NO EXISTEN EN LA DB, LO AGREGA AUTOMATICAMENTE.
 model.sequelize.sync().then((xx)=>{
     server.listen(process.env.PORT, () => {
         console.log(`Servidor corriendo en el puerto : ${process.env.PORT}` );
